0368f247a3ac5b6bcfaa90d04a366194bf1c4bd2
Christian Fleschhut <weltrekordhalter@gmail.com>
2014-01-09 14:49:30 +0100

    include #logo a to collapse .navbar-collapse on click

--- source/javascripts/navbar.js
+++ source/javascripts/navbar.js
@@ -5,9 +5,18 @@ $(document).ready(function() {
     var mq = window.matchMedia("(max-width: 767px)");
     var mediaQueryChange = function(mq) {
       if (mq.matches) {
-        $(".navbar-collapse").on("click", "a", function(ev) {
-          $(ev.delegateTarget).collapse("hide");
+
+        $(".navbar").on("click", "a", function(ev) {
+          var navbarCollapse = $(ev.delegateTarget).find(".navbar-collapse");
+          if (navbarCollapse.hasClass("in")) {
+            navbarCollapse.collapse("hide");
+          }
         });
+
+      } else {
+
+        $(".navbar").off("click", "a");
+
       }
     };
     mq.addListener(mediaQueryChange);
