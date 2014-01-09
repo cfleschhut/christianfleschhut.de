ac04c5d96f1aa630c5bc4cd45e00991b50ff4116
Christian Fleschhut <weltrekordhalter@gmail.com>
2014-01-09 16:25:30 +0100

    small-screen navbar collapse: handlerFn

--- source/javascripts/navbar.js
+++ source/javascripts/navbar.js
@@ -4,20 +4,20 @@ $(document).ready(function() {
   if (window.matchMedia) {
     var mq = window.matchMedia("(max-width: 767px)");
     var mediaQueryChange = function(mq) {
+
+      var handlerFn = function(ev) {
+        var navbarCollapse = $(ev.delegateTarget).find(".navbar-collapse");
+        if (navbarCollapse.hasClass("in")) {
+          navbarCollapse.collapse("hide");
+        }
+      };
+
       if (mq.matches) {
-
-        $(".navbar").on("click", "a", function(ev) {
-          var navbarCollapse = $(ev.delegateTarget).find(".navbar-collapse");
-          if (navbarCollapse.hasClass("in")) {
-            navbarCollapse.collapse("hide");
-          }
-        });
-
+        $(".navbar").on("click", "a", handlerFn);
       } else {
-
         $(".navbar").off("click", "a");
-
       }
+
     };
     mq.addListener(mediaQueryChange);
     mediaQueryChange(mq);
